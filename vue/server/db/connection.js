96edce2b4fc98767c6bc1c0c48c5e1d40aae4764
userName2020 <xiuer_csdn@163.com>
2020-09-23 19:13:06 +0800

    分类完成

--- vue/server/db/connection.js
+++ vue/server/db/connection.js
@@ -2,6 +2,9 @@ const Sequelize = require('sequelize');
 //连接mysql数据， ORM层
 
 module.exports = new Sequelize(config.databaseOptions.database, config.databaseOptions.user, config.databaseOptions.password, {
+  // config.databaseOptions.database： 数据库名称
+  // config.databaseOptions.user ：    用户名
+  // config.databaseOptions.password： 用户密码
 
   host: config.databaseOptions.host,
   //数据库地址

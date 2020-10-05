9185b996a0dd61f157cea081992f4e6509d8cf3e
“xuserNamegit config --global user.email “xxiuer_csdn@163.com <xiuer_csdn@163.com>
2020-10-05 20:11:31 +0800

    添加vuex的modules模板

---
+++ vue/manager/src/store/modules/demo.js
@@ -0,0 +1,24 @@
+export default {
+    namespaced: true,
+    // 成为带命名空间的模块
+    state: () => ({
+        
+    }),
+    mutations: {
+        
+    },
+    // 接收的第一个参数是模块的局部state
+    actions: {
+        
+    },
+    // 第一个参数context 常用对象结构 { dispatch, commit, getters, rootGetters }
+    // 第二个参数 payload载荷
+    getters: {
+        
+    }
+    // 第一个参数: 模块的局部state
+    // 第二个参数: 模块的getter
+    // 第三个参数: 根节点state
+    // 第四个参数: 根节点getter
+    
+}

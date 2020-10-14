249449d2d3a0acb567ef22b66619658fd3f0f211
“xuserNamegit config --global user.email “xxiuer_csdn@163.com <xiuer_csdn@163.com>
2020-10-14 16:24:12 +0800

    增加创建数据,修改数据,查询数据路由

--- server/router/router.js
+++ server/router/router.js
@@ -4,6 +4,8 @@
 const routerController = require(__basename + '/router_controller/routerController.js');
 
 module.exports = app => {
-  
+  app.post("/createData", routerController.createData)
+  app.post("/updateData", routerController.updateData)
+  app.post("/findAllData", routerController.findAllData)
 }
 // 导出方法
--- server/router_controller/routerController.js
+++ server/router_controller/routerController.js
@@ -3,9 +3,40 @@
 let api = require(__basename + '/api/api.js');
 //导入api(服务)层，操作数据库
 
+const {
+    Op
+} = require("sequelize");
 class RouterController {
-  
+    createData(req, res) {
+        api.createData("Demo", {
+            demo: "buildData"
+        }).then(() => {
+            res.send({
+                msgText: "创建数据成功"
+            })
+        });
 
+    }
+    updateData(req, res) {
+        let id = req.body.id;
+        let arr = api.updateData("Demo", {
+            demo: "updateData"
+        }, {
+            id
+        })
+        res.send(arr)
+    }
+    findAllData(req, res) {
+        api.findAllData("Demo", {
+            id: {
+                [Op.gt]: 1
+            }
+        }).then(result => {
+            console.log("result ==> ", result);
+            res.send(result)
+        })
+
+    }
 }
 
 

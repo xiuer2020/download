bde4bab28108887721564f851da7ca8da3f2657f
“xuserNamegit config --global user.email “xxiuer_csdn@163.com <xiuer_csdn@163.com>
2020-10-14 16:22:29 +0800

    增加数据库表的增,改,查方法

--- server/api/api.js
+++ server/api/api.js
@@ -1,12 +1,40 @@
 //服务层
 
 class API {
-  createData(modelName, o) {
+  createData(modelName, obj) {
     //modelName: 模型名称, string
-    //o: 创建的数据, object
-    return model[modelName].create(o);
+    //obj: 创建的数据, object
+    return model[modelName].create(obj);
   }
-  //添加数据
+  //添加数据 返回promise
+
+  updateData(modelName, obj, where) {
+    //modelName: 模型名称, string
+    //obj: 创建的数据, object
+    // where: 查询条件
+    return model[modelName].update(obj, {
+      where
+    });
+  }
+  //更新数据 无配置
+
+  findAllData(modelName, where, attributes, offset = 0, limit, order = ["id"]){
+    //modelName: 模型名称, string
+    //where: 查询条件, object
+    //attributes: 查询字段, array
+    // order: 排序依据, array
+    // limit: 查询数量 number
+    // offset 结果偏移量 number
+    
+    return model[modelName].findAll({
+      where,
+      attributes,
+      order,
+      offset,
+      limit: 3
+    })
+  }
+  //查询数据 返回promiseu
 }
 // 数据的增删改方法
 

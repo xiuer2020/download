abdda7c09943b822b06ccec6271325e00aaceff9
“xuserNamegit config --global user.email “xxiuer_csdn@163.com <xiuer_csdn@163.com>
2020-10-14 08:49:15 +0800

    清空路由

--- server/router/router.js
+++ server/router/router.js
@@ -4,7 +4,6 @@
 const routerController = require(__basename + '/router_controller/routerController.js');
 
 module.exports = app => {
-  app.get('/send', routerController.send);
-  // 提交
+  
 }
 // 导出方法

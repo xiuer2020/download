8efe8eb143074f9665fbcf1228769d4ae75286f2
“xuserNamegit config --global user.email “xxiuer_csdn@163.com <xiuer_csdn@163.com>
2020-10-14 08:49:52 +0800

    清空路由控制器

--- server/router_controller/routerController.js
+++ server/router_controller/routerController.js
@@ -4,14 +4,8 @@ let api = require(__basename + '/api/api.js');
 //导入api(服务)层，操作数据库
 
 class RouterController {
-  send(req, res){
-   api.createData('Call', {
-     name: req.query.name,
-     email: req.query.email,
-     description: req.query.description
-   }) 
-    res.send({msg: '提交成功', code: 200})
-  }
+  
+
 }
 
 

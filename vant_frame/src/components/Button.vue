86228eafeba7014c4d055bcfbd39cc628dc45731
pzfUserName <xiuer_csdn@163.com>
2021-04-09 17:45:43 +0800

    '4/9'

---
+++ vant_frame/src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="demo">
+    <div class="van-button van-button--default van-button-normal van-button--loading">
+      <div class="van-button_content">
+        <!--      ::before-->
+        loading slot
+      </div>
+      <!--    df + aic + h100p-->
+    </div>
+    <!--  loading状态-->
+
+    <div class="van-button van-button--default van-button-normal van-button--loading">
+<!--      ::before-->
+      <div class="van-button_content">
+        <!--      ::before-->
+        loading slot
+      </div>
+      <!--    df + aic + h100p-->
+    </div>
+    <!--  普通状态-->
+
+  </div>
+</template>
+<script>
+  export default {}
+</script>
+<style lang="less">
+
+
+</style>

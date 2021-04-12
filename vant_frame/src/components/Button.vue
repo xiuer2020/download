078dd110bc68e754f90388262ce0cf0e3e3a49d5
pzfUserName <xiuer_csdn@163.com>
2021-04-12 17:54:46 +0800

    4/12

--- vant_frame/src/components/Button.vue
+++ vant_frame/src/components/Button.vue
@@ -1,30 +1,32 @@
 <template>
   <div class="demo">
-    <div class="van-button van-button--default van-button-normal van-button--loading">
+    <div
+      class="van-button van-button--default van-button-normal van-button--loading"
+    >
       <div class="van-button_content">
         <!--      ::before-->
         loading slot
       </div>
       <!--    df + aic + h100p-->
     </div>
-    <!--  loading状态-->
+    <!--  loading状态 h44 + p + tac-->
 
-    <div class="van-button van-button--default van-button-normal van-button--loading">
-<!--      ::before-->
+    <div
+      class="van-button van-button--default van-button-normal van-button--loading"
+    >
+      <!--      ::before-->
       <div class="van-button_content">
         <!--      ::before-->
-        loading slot
+        default slot
       </div>
       <!--    df + aic + h100p-->
     </div>
-    <!--  普通状态-->
+    <!--  普通状态 h44 + p + tac-->
 
   </div>
 </template>
 <script>
-  export default {}
+export default {};
 </script>
 <style lang="less">
-
-
 </style>
